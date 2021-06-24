62f8411b343312c36068aa8ce82f95386d5a0941
BehzadShayegh <behzad.shayegh@ut.ac.ir>
2021-06-24 18:02:30 +0430

    Rename 'CSE' to 'Offering'

--- src/domain/EnrollCtrl.java
+++ src/domain/EnrollCtrl.java
@@ -6,16 +6,16 @@ import java.util.Map;
 import domain.exceptions.EnrollmentRulesViolationException;
 
 public class EnrollCtrl {
-	public void enroll(Student s, List<CSE> courses) throws EnrollmentRulesViolationException {
+	public void enroll(Student s, List<Offering> courses) throws EnrollmentRulesViolationException {
         Map<Term, Map<Course, Double>> transcript = s.getTranscript();
         checkExceptions(courses, transcript);
-		for (CSE o : courses)
+		for (Offering o : courses)
 			s.takeCourse(o.getCourse(), o.getSection());
 	}
 
-    private void checkExceptions(List<CSE> courses, Map<Term, Map<Course, Double>> transcript) throws EnrollmentRulesViolationException {
+    private void checkExceptions(List<Offering> courses, Map<Term, Map<Course, Double>> transcript) throws EnrollmentRulesViolationException {
 		String errors = "";
-        for (CSE o : courses) {
+        for (Offering o : courses) {
             try {checkIfAlreadyPassed(o, transcript);}
             catch(Exception e) {errors += e.toString()+"\n";}
 
@@ -37,8 +37,8 @@ public class EnrollCtrl {
         }
     }
 
-    private void checkExamTimeCollision(CSE o, List<CSE> courses) throws EnrollmentRulesViolationException {
-        for (CSE o2 : courses) {
+    private void checkExamTimeCollision(Offering o, List<Offering> courses) throws EnrollmentRulesViolationException {
+        for (Offering o2 : courses) {
             if (o == o2)
                 continue;
             if (o.getExamTime().equals(o2.getExamTime()))
@@ -46,8 +46,8 @@ public class EnrollCtrl {
         }
     }
 
-    private void checkDuplicateRequest(CSE o, List<CSE> courses) throws EnrollmentRulesViolationException {
-        for (CSE o2 : courses) {
+    private void checkDuplicateRequest(Offering o, List<Offering> courses) throws EnrollmentRulesViolationException {
+        for (Offering o2 : courses) {
             if (o == o2)
                 continue;
             if (o.getCourse().equals(o2.getCourse()))
@@ -55,7 +55,7 @@ public class EnrollCtrl {
         }
     }
 
-    private void checkPrerequisites(CSE o, Map<Term, Map<Course, Double>> transcript) throws EnrollmentRulesViolationException {
+    private void checkPrerequisites(Offering o, Map<Term, Map<Course, Double>> transcript) throws EnrollmentRulesViolationException {
         List<Course> prereqs = o.getCourse().getPrerequisites();
         nextPre:
         for (Course pre : prereqs) {
@@ -69,7 +69,7 @@ public class EnrollCtrl {
         }
     }
 
-    private void checkIfAlreadyPassed(CSE o, Map<Term, Map<Course, Double>> transcript) throws EnrollmentRulesViolationException {
+    private void checkIfAlreadyPassed(Offering o, Map<Term, Map<Course, Double>> transcript) throws EnrollmentRulesViolationException {
         for (Map.Entry<Term, Map<Course, Double>> tr : transcript.entrySet()) {
             for (Map.Entry<Course, Double> r : tr.getValue().entrySet()) {
                 if (r.getKey().equals(o.getCourse()) && r.getValue() >= 10)
@@ -78,9 +78,9 @@ public class EnrollCtrl {
         }
     }
 
-    private void checkUnitsLimitation(List<CSE> courses, Map<Term, Map<Course, Double>> transcript) throws EnrollmentRulesViolationException {
+    private void checkUnitsLimitation(List<Offering> courses, Map<Term, Map<Course, Double>> transcript) throws EnrollmentRulesViolationException {
         int unitsRequested = 0;
-		for (CSE o : courses)
+		for (Offering o : courses)
 			unitsRequested += o.getCourse().getUnits();
         double gpa = calculateGpa(transcript);
 		if ((gpa < 12 && unitsRequested > 14) ||
